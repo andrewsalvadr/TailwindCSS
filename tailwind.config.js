35437581b8ac69b869f29498f4d46491351cd435
Andrew Salvador <andrewsalva88@gmail.com>
2022-07-22 23:35:24 +0800

    tailwind html

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+    theme: {
+    extend: {
+      fontFamily: {
+        'roboto': ["Roboto, sans-serif;"],
+        'pops': ["Poppins, sans-serif;"],
+      },
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px'
+      },
+    },
+  },
+  plugins: [],
+}
